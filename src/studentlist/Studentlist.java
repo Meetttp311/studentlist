99707cbe0638e3c1886633887ab241a4fd29c5dd
pate3857 <pate3857@sheridancollege.ca>
2023-05-23 14:32:22 -0400

    The correct code

---
+++ src/studentlist/Studentlist.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+/**
+ *
+ * @author pate3857
+ */
+public class Studentlist {
+    private String name;
+    private String studentID;
+    private String address;
+    
+    public Student(String givenName)
+    {
+        name = givenname;
+
+    }
+}
